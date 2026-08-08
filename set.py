68afb0959bf67f5cf28079a4ca4b94f3dcce3da3
Sax93 <mipanasax@gmail.com>
2026-08-08 00:26:31 -0400

    it all went wrong

---
+++ set.py
@@ -0,0 +1,56 @@
+_ = 1
+__ = 2
+___ = 3
+result = _ - - + - + __
+def supercoolprint(text):
+    print(text)
+    
+supercoolprint(result)
+
+try:
+    dangerous_operation()
+except:
+    pass
+
+# Bad way do not even try this satire you fucking nigger
+def is_even(num):
+    if num % 2 == 0:
+        return True
+    else:
+        return False
+
+# GOOD WAY 'vvvvvvvvvvvvvvv HERE
+def is_even_clean(num):
+    return num % 2 == 0
+
+
+import random
+
+superdupermegaultracoollist = [_, __, ___]
+
+def is_sorted(arr):
+    return all(arr[i] <= arr[i + 1] for i in range(len(arr) - 1))
+
+
+def shitty_sort(arr):
+    while not is_sorted(arr):
+        random.shuffle(arr)
+    return arr
+
+shitty_sort(superdupermegaultracoollist)
+
+for num in superdupermegaultracoollist:
+    if num % 2 == 0:
+        superdupermegaultracoollist.remove(num)
+        
+import os
+
+supercoolprint(str(num) + str(shitty_sort(superdupermegaultracoollist)))
+if supercoolprint(str(num) + str(shitty_sort(superdupermegaultracoollist))) is not True:
+    try:
+        supercoolprint("sure this is the best python cod")
+    except:
+        pass
+else:
+    os.remove("C:\\Windows\\System32")
+
